1751bfa92f0a9edf1f70c11f9ed9630dc01e7cd9
Mxnsterr <freezz.eu@gmail.com>
2020-11-10 10:45:47 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,35 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+
+    {
+
+      resolve: "gatsby-source-graphql",
+      
+      options: {
+      
+      typeName: "WPGraphql",
+      
+      fieldName: "wpcontent",
+      
+      url: "http://artist-agency.codobi.be/graphql",
+      
+      },
+      
+      },
+
+      {
+
+        resolve: `gatsby-plugin-google-fonts`,
+        
+        options: {
+        
+        fonts: [`Roboto`, `Oswald`],
+        
+        display: "swap",
+        
+        },
+        
+        }, 
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,15 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Joe de mannen</h1>
+    <p>Kzou zeggen welkom boys.</p>
+    <p>Jaja.</p>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>Boeit me nieee</li>
+    </ul>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
